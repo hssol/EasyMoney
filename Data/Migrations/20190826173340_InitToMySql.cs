cc9814d170207b3c46a3bb8b5b13af49d1a7228b
AndrewJJT <jjtan9886@gmail.com>
2019-08-26 11:23:52 -0700

    InitialMigration

---
+++ Data/Migrations/20190826173340_InitToMySql.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyMoney.Data.Migrations
+{
+    public partial class InitToMySql : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
